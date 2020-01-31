3c65f54a376ad6cd5877e0b1db461ab778633321
Akira Taguchi <akirahattivatti@gmail.com>
2020-01-31 10:31:54 +0200

    postgresql implemented

--- application/__init__.py
+++ application/__init__.py
@@ -1,15 +1,19 @@
-# flask-sovellus
 from flask import Flask
 app = Flask(__name__)
 
-# tietokanta
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///courseplan.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///courseplan.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
 
 db = SQLAlchemy(app)
 
-# oman sovelluksen toiminnallisuudet
 from application import views
 
 from application.students import models
@@ -18,7 +22,6 @@ from application.students import views
 from application.auth import models
 from application.auth import views
 
-# kirjautuminen
 from application.auth.models import User
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
@@ -33,6 +36,7 @@ login_manager.lgin_message = "Please login to use this functionality."
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
-# luodaan taulut tietokantaan tarvittaessa
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from application import app
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(loging.ERROR)
 
 if __name__ == '__main__':
     app.run(debug=True)
