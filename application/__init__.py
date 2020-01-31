f870769adb5b6f289111edae1612dd7301076602
Akira Taguchi <akirahattivatti@gmail.com>
2020-01-31 11:09:27 +0200

    typo in init.py

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 if os.environ.get("HEROKU"):
-    app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///courseplan.db"
     app.config["SQLALCHEMY_ECHO"] = True
